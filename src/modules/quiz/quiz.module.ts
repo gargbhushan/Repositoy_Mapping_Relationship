e7071ff3111d16476d8657e493eb755a221179fd
Bhushan Kumar <64317280+gargbhushan@users.noreply.github.com>
2022-07-27 15:27:47 +0530

    Add files via upload

---
+++ src/modules/quiz/quiz.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { QuestionController } from './controller/question.controller';
+import { QuizController } from './controller/quiz.controller';
+import { QuestionRepository } from './repository/question.repository';
+import { QuizRepository } from './repository/quiz.repository';
+import { QuestionService } from './service/question.service';
+import { QuizService } from './service/quiz.service';
+
+
+@Module({
+    controllers:[QuizController,QuestionController],
+    imports:[TypeOrmModule.forFeature([QuizRepository,QuestionRepository])],
+    providers:[QuizService,QuestionService],
+})
+export class QuizModule {}
